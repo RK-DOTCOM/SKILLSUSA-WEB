734dc8c5b585bfd95c41b65bf1a8db63e38eae96
willwan8 <williamwang1562@gmail.com>
2024-12-01 22:12:50 -0600

    added paths to routes in router.js and added a 404.html to pages

--- router.js
+++ router.js
@@ -1,4 +1,8 @@
 //contains possible paths as keys and html files that need to be displayed according to the paths
 const routes = {
-
-}
+  404: "/pages/404.html",
+  "/": "/pages/home.html",
+  "/about": "/pages/about.html",
+  "/contact-us": "/pages/contact.html",
+  "/join-us": "/pages/join.html",
+};
